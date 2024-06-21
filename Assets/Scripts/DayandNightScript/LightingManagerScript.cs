dcb6988d7e143ced6162b5ebe9438b5b37eb9d6d
deepanshu <deepanshu120100@gmail.com>
2024-06-21 11:00:53 -0600

    dayandnight
    basic day and night cycle setup later ill continue on it

---
+++ Assets/Scripts/DayandNightScript/LightingManagerScript.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using LightType = UnityEngine.LightType;
+using RenderSettings = UnityEngine.RenderSettings;
+
+//it can make it edited while not playing the game but also be able to see the changes
+[ExecuteAlways]
+
+public class LightingManagerScript : MonoBehaviour
+{
+    [SerializeField]
+    private Light DirectionalLight;
+    [SerializeField]
+    private LightingPresetScript preset;
+    [SerializeField, Range(0,24)]
+    private float DayTime;
+
+    private void Update()
+    {
+        //to set the angle
+
+        if(preset == null)
+        {
+            return;
+            //if its not assigned
+        }
+
+        //setting it if the time is playing
+
+        if(Application.isPlaying) 
+        {
+            DayTime += Time.deltaTime;
+            DayTime %= 24;
+            //setting it between 0-24
+            UpdateLighting(DayTime / 24f);
+            
+        }
+        else
+        {
+
+            UpdateLighting(DayTime / 24f);
+           
+        }
+
+    }
+    private void UpdateLighting(float timeper)
+        {
+
+         RenderSettings.ambientLight = preset.Ambientcolor.Evaluate(timeper);
+         RenderSettings.fogColor = preset.FogColor.Evaluate(timeper);
+
+        //to check we got the light or not then color and rotation set
+
+        if(DirectionalLight != null) 
+        {
+            DirectionalLight.color = preset.DirectionalColor.Evaluate(timeper);
+
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timeper * 360f) - 90f, -170, 0));
+        }
+    }
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+        {
+            return;
+        }
+        //to find the first light in the scene we can get
+
+        if (RenderSettings.sun!=null) 
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach(Light light in lights)
+            {
+                if(light.type == LightType.Directional) 
+                {
+                 DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+   
+}
